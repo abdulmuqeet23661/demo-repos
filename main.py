826adafddaa3ed47586280646bf7884328e2deb4
Abdul Muqeet Waheed <63511515+abdulmuqeet23661@users.noreply.github.com>
2023-01-09 14:32:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from PyQt5 import QtWidgets, QtCore
+from mainwindow import MainWindow
+
+
+if __name__ == "__main__":
+
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    cwind = MainWindow()
+    cwind.show()
+
+
+    sys.exit(app.exec_())
